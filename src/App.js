31640f94382dde51f6ab2b71a7341e2cc37fbe18
Darth Chorus <jodyr087@gmail.com>
2025-07-05 15:19:26 -0700

    Your commit message describing changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Shop from "./Shop.js";
 import AddMerchPage from "./AddMerchPage.js";
 import Cart from "./Cart.js";
 
-
 function App() {
   const requireLogin = false;
   return (
@@ -17,54 +16,54 @@ function App() {
       <Router>
         <Header />
         <Routes>
-  {requireLogin ? (
-    // --- Login protection ON ---
-    <>
-      <Route path="/" element={<LoginPage />} />
-      <Route
-        path="/HomePage"
-        element={
-          <ProtectedRoute>
-            <HomePage />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/Shop"
-        element={
-          <ProtectedRoute>
-            <Shop />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/AddMerchPage"
-        element={
-          <ProtectedRoute>
-            <AddMerchPage />
-          </ProtectedRoute>
-        }
-      />
-      <Route
+          {requireLogin ? (
+            // --- Login protection ON ---
+            <>
+              <Route path="/" element={<LoginPage />} />
+              <Route
+                path="/HomePage"
+                element={
+                  <ProtectedRoute>
+                    <HomePage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/Shop"
+                element={
+                  <ProtectedRoute>
+                    <Shop />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/AddMerchPage"
+                element={
+                  <ProtectedRoute>
+                    <AddMerchPage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
                 path="/cart"
                 element={
                   <ProtectedRoute>
                     <Cart />
                   </ProtectedRoute>
                 }
-    />
-    </>
-  ) : (
-    // --- Login protection OFF ---
-    <>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/HomePage" element={<HomePage />} />
-      <Route path="/Shop" element={<Shop />} />
-      <Route path="/AddMerchPage" element={<AddMerchPage />} />
-      <Route path="/cart" element={<Cart />} />
-    </>
-  )}
-</Routes>
+              />
+            </>
+          ) : (
+            // --- Login protection OFF ---
+            <>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/HomePage" element={<HomePage />} />
+              <Route path="/Shop" element={<Shop />} />
+              <Route path="/AddMerchPage" element={<AddMerchPage />} />
+              <Route path="/cart" element={<Cart />} />
+            </>
+          )}
+        </Routes>
 
         <Footer />
       </Router>
